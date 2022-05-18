01e58ae91fd75aa6d04589e9605b68dcd4713616
VictorUlian <yctor.ulian@gmail.com>
2022-05-18 15:57:19 -0300

    Feature: Now can POST at endpoint poll

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from "dotenv";
 import { MongoClient} from "mongodb";
+import dayjs from 'dayjs'
 
 
 dotenv.config();
@@ -10,6 +11,34 @@ app.use(express.json());
 app.use(cors());
 const mongoClient = new MongoClient(process.env.MONGO_URI);
 
+app.post('/poll', async (req, res) => {
+    const { title} = req.body;
+    let {expireAt} = req.body;
+
+    if(!expireAt){
+        let expiration = dayjs().add(30, 'day').format("YYYY-MM-DD HH:mm").toString()
+        expireAt = expiration
+        console.log(expireAt);
+    }
+    if(!title){
+        console.log("titulo nao pode ser vazio")
+        res.status(422).send("titulo nao pode ser vazio");
+    }
+    try {
+        await mongoClient.connect()
+        const db = mongoClient.db(process.env.DATABASE);
+        await db.collection("poll").insertOne({
+            title: title,
+            expireAt: expireAt
+        });
+        res.status(201).send("Usuario cadastrado com sucesso");
+    } catch (e) {
+        console.error(e);
+        res.sendStatus(422);
+    }
+    console.log(expireAt);
+    res.status(201).send("Criado com sucess")
+});
 
 
 
