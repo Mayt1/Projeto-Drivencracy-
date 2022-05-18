f5b4cd682c657f14baf890103f21c2abbceaf52b
VictorUlian <yctor.ulian@gmail.com>
2022-05-18 16:36:12 -0300

    Feature: Now can POST at endpoint /choice, which will add the functionality of creating a choice in the poll

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from "dotenv";
-import { MongoClient} from "mongodb";
+import { MongoClient, ObjectId} from "mongodb";
 import dayjs from 'dayjs'
 
 
@@ -55,6 +55,43 @@ app.get("/poll", async (req, res) => {
     } catch (e) {
         console.error(e);
         return res.sendStatus(422);
+    }
+});
+
+app.post('/choice', async (req, res) => {
+    const { title, poolId} = req.body;
+    if(!title){
+        console.log("titulo nao pode ser vazio")
+        res.status(422).send("titulo nao pode ser vazio");
+    }
+    try {
+        await mongoClient.connect()
+        const db = mongoClient.db(process.env.DATABASE);
+        const poll = await db.collection("poll").findOne({_id: new ObjectId(poolId)})
+        const isTitleAtPoll = await db.collection("choice").findOne({title: title})
+        if(poll.expireAt){
+            //let validador = dayjs().isBefore(poll.expireAt)
+            let validador = dayjs().isBefore(poll.expireAt)
+            if(validador){
+                console.log("enquete aberta")
+            } else {
+                res.status(403).send("enquete já expirada")
+            }
+        }
+        if(!poll){
+            res.status(404).send("nao existe esta enquete")
+        }
+        if(isTitleAtPoll){
+            res.status(409).send("Esta opçao ja existe")
+        } 
+        await db.collection("choice").insertOne({
+            title: title,
+            poolId: poolId
+        });
+        res.status(201).send("Escolha cadastrada com sucesso");
+    } catch (e) {
+        console.error(e);
+        res.sendStatus(422);
     }
 });
 
